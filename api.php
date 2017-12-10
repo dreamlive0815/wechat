ac5fd4f27f75924729a418c458083ca69641b2e7
unknown <>
2017-12-10 22:54:24 +0800

    201712102254

--- api.php
+++ api.php
@@ -1,2 +1,21 @@
 <?php
-print_r( $_REQUEST );
+
+require( 'autoload.php' );
+
+use Util\CommonUtil as CU;
+use Util\ErrorUtil as EU;
+use Util\FilterUtil as FU;
+
+EU::setGlobalExceptionHandler();
+
+FU::addFilter( 'exceptionHandler', function( $info ) {
+    echo $info['exception']->getMessage();
+    return $info;
+}, 999 );
+
+$controller = CU::getR( 'controller' );
+$action = CU::getR( 'action' );
+
+$classname = "\\Control\\{$controller}Controller";
+$controllerInstance = new $classname();
+
