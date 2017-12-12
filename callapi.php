de4161cc1f2d6864e1843bf8fd87c50d7772c4ef
unknown <>
2017-12-12 14:50:25 +0800

    201712121450

--- callapi.php
+++ callapi.php
@@ -1,13 +1,15 @@
 <?php
 
-$config = require( 'config.php' );
+$config_common = require( 'config_common.php' );
+$config_wechat = require( 'config_wechat_beta.php' );
 
-include $baseDir . '/vendor/autoload.php';
+$wechatSDKDir = $config_common['wechatSDKDir'];
+require( $wechatSDKDir . '/vendor/autoload.php' );
 
 use EasyWeChat\Foundation\Application;
 use EasyWeChat\Message\Text;
 
-$app = new Application( $config );
+$app = new Application( $config_wechat );
 $staff = $app->staff;
 $openid = 'oQ4KVw14cKQ4lucVr4N8mJNY_Cro';
 $message = new Text( [ 'content' => 'Hello world!' ] );
@@ -18,5 +20,5 @@ try
 }
 catch(\Exception $ex)
 {
-    print_r( $ex );
+    print_r( $ex->getMessage() );
 }
