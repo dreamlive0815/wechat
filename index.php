1659c32f3a0133191fa76ec0128ba9467f34ab67
unknown <>
2017-12-12 16:10:03 +0800

    201712121609

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
 <?php
 
-$config_common = require( 'config_common.php' );
-$config_wechat = require( 'config_wechat_beta.php' );
+require( './wechat_head.php' );
 
-$wechatSDKDir = $config_common['wechatSDKDir'];
-require( $wechatSDKDir . '/vendor/autoload.php' );
-
-use EasyWeChat\Foundation\Application;
-
-$app = new Application( $config_wechat );
 $server = $app->server;
 $response = $server->serve();
 $response->send();
