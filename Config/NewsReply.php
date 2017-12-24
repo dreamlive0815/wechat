38a10d21588aa9003c7b375e15adfc16a0e9792f
unknown <>
2017-12-24 23:29:29 +0800

    2017122242329

--- Config/NewsReply.php
+++ Config/NewsReply.php
@@ -8,10 +8,44 @@ class NewsReply extends Config
         'key' => 'Touhou',
         'newsArray' => [
             [ 
-                'title' => '东方', 
+                'title' => '东方Project',
+                'url' => 'http://edu.zstusunshine.cn/wechat/Resource/Image/59888608_p0.jpg',
+                'image' => 'http://edu.zstusunshine.cn/wechat/Resource/Image/59888608_p0.jpg',
             ],
             [
-                'title' => '东方Project',
+                'title' => 'Bad Apple!!',
+                'url' => 'https://www.bilibili.com/video/av706/',
+                'image' => 'http://edu.zstusunshine.cn/wechat/Resource/Image/BA-1645.jpg',
+            ],
+            [
+                'title' => '东方是什么',
+                'url' => 'https://www.bilibili.com/video/av3388284/',
+                'image' => 'http://i0.hdslb.com/bfs/archive/beb7c676f70e45ce30e0b8ac16d366976e0bbe0b.jpg@380w_240h_75Q_1c.webp',
+            ],
+            [
+                'title' => '韵墨东方',
+                'url' => 'https://www.bilibili.com/video/av611261/',
+                'image' => 'http://i2.hdslb.com/bfs/archive/33630327773897e0f543b07fbdcab6f753cb6ed6.jpg@380w_240h_75Q_1c.webp',
+            ],
+            [
+                'title' => '梦想夏乡',
+                'url' => 'https://www.bilibili.com/video/av6005511/',
+                'image' => 'http://i2.hdslb.com/bfs/archive/d09baacb7062ccd9ed56c08d0fc3134dd6aa5571.jpg@380w_240h_75Q_1c.webp',
+            ],
+            [
+                'title' => '花鳥風月',
+                'url' => 'https://www.bilibili.com/video/av7732869/',
+                'image' => 'http://i1.hdslb.com/bfs/archive/a1d5cca5e1a04816a36a0d042f769c72aacce8c9.jpg@380w_240h_75Q_1c.webp',
+            ],
+            [
+                'title' => '【3D东方】灵梦vs幽幽子',
+                'url' => 'https://www.bilibili.com/video/av917506/',
+                'image' => 'http://i1.hdslb.com/bfs/archive/a0a3d682c401d008c8fedbb5805a5e8519dc6186.jpg@380w_240h_75Q_1c.webp',
+            ],
+            [
+                'title' => '【3D东方】魔理沙vs阿空',
+                'url' => 'https://www.bilibili.com/video/av10021507/',
+                'image' => 'http://i0.hdslb.com/bfs/archive/00302979e207a8315d2c72f64341e2b6283ca113.jpg@380w_240h_75Q_1c.webp',
             ]
         ],
     ];
