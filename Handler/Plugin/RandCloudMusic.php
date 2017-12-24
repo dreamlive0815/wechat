60fb1c2bf1efe6935b007e5858dab3948c9808d5
unknown <>
2017-12-24 14:29:02 +0800

    201712241429

--- Handler/Plugin/RandCloudMusic.php
+++ Handler/Plugin/RandCloudMusic.php
@@ -11,18 +11,20 @@ class RandCloudMusic extends Plugin
 {
     function run( $text )
     {
-        if( preg_match( '/^(音乐|music)$/i', $text, $match ) )
+        if( preg_match( '/^(音乐|music)(\s+link)?$/i', $text, $match ) )
         {
             $setting = Setting::getSetting( [ 'openid' => $this->openid, 'type' => 'cloud_user_id' ] );
-            $user_id = '352148975';
+            $u_id = $user_id = '352148975';
             if( $setting->id ) $user_id = $setting->data;
-            $records = CM::getUserWeekPlayRecordInfo( $user_id );
+            try { $records = CM::getUserWeekPlayRecordInfo( $user_id ); }
+            catch( \Exception $ex ) { $records = CM::getUserWeekPlayRecordInfo( $u_id ); }
             $cnt = count( $records );
             $index = mt_rand( 0, $cnt - 1 );
             $song = $records[$index]['song'];
             $url = CM::getSongURLInfo( $song['id'] );
             $url = sprintf( '%s/%s/View/download.Cloud.php?id=%s', EU::getServerBaseURL(), Config::basename, $song['id'] );
             $this->handled = true;
+            if( isset( $match[2] ) ) return $url;
             return $this->getNews( [
                 'title' => $song['name'],
                 'description' => '点击下载',
